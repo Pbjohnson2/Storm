4679fb6a437eb856d8564fcf7f39a54f24ddacd2
Pierce Johnson <Pierce@TheChill2.(none)>
2015-06-14 14:59:03 -0700

    Add initial files for project

---
+++ src/main/java/com/swerve/storm/AuthorizationActivity.java
@@ -0,0 +1,13 @@
+package com.swerve.storm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AuthorizationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorization);
+    }
+}
